b44266aedb57452e0e0fc19cd94148d4deb2b3a1
Alejandro <rangel.celis@gmail.com>
2023-10-03 12:06:56 -0300

    Initial commit.

---
+++ cypress/e2e/Practice/cypress-commands.cy.js
@@ -0,0 +1,22 @@
+describe('Custom Cypress Commands', () => {
+  it('uses cy.getAllPosts()to retrieve all posts from the /api/posts endpoint', () => {
+    // Create a custom Cypress Command called 'getAllPosts' which uses
+    // cy.request() to get all of the posts from the /api/posts endpoint
+    // Then use this custom command in this test to assert that the length of the posts
+    // returned is equal to 2
+    cy.getAllPosts().then((posts) => {
+      cy.wrap(posts).its('length').should('eq', 2);
+    });
+  });
+
+  it('uses cy.getFirstPost() to retrieve the first post from the /api/posts endpoint', () => {
+    // Create a custom Cypress Command called 'getFirstPost' which uses
+    // cy.request() to get the first post from the /api/posts endpoint
+    // Then use this custom command in this test to assert that the id of the first post
+    // is equal to 'pre-rendering'
+    cy.getFirstPost().then((post) => {
+      // expect(response).to.have.property('id', 'pre-rendering');
+      cy.wrap(post).its('id').should('eq', 'pre-rendering');
+    });
+  });
+});
